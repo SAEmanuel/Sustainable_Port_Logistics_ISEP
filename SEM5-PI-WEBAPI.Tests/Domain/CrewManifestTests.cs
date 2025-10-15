390baeed96163c5bfda70a5b362b5471e50d1800
Francisco Santos <1230564@isep.ipp.pt>
2025-10-15 16:15:42 +0100

    CrewManifest tests added Issue: #14

---
+++ SEM5-PI-WEBAPI.Tests/Domain/CrewManifestTests.cs
@@ -0,0 +1,55 @@
+using SEM5_PI_WEBAPI.Domain.CrewManifests;
+using SEM5_PI_WEBAPI.Domain.CrewMembers;
+using SEM5_PI_WEBAPI.Domain.Shared;
+
+
+namespace SEM5_PI_WEBAPI.Tests.Domain;
+
+public class CrewManifestTests
+{
+    private readonly List<CrewMember> _dummyCrewMembers = new List<CrewMember>
+    {
+        new ("Alice", CrewRole.Cook, Nationality.Qatar, new CitizenId("12345AA")),
+        new ("Bob", CrewRole.EngineCadet, Nationality.Angola, new CitizenId("67890BB"))
+    };
+
+    [Fact]
+    public void CreateCrewManifest_ValidData_ShouldInitialize()
+    {
+        int totalCrew = 2;
+        string captainName = "Captain Nemo";
+
+        var crewManifest = new CrewManifest(totalCrew, captainName, _dummyCrewMembers);
+
+        Assert.NotNull(crewManifest.Id);
+        Assert.Equal(totalCrew, crewManifest.TotalCrew);
+        Assert.Equal(captainName, crewManifest.CaptainName);
+        Assert.Equal(_dummyCrewMembers, crewManifest.CrewMembers);
+    }
+
+    [Fact]
+    public void CreateCrewManifest_NegativeTotalCrew_ShouldThrow()
+    {
+        Assert.Throws<BusinessRuleValidationException>(() =>
+            new CrewManifest(-1, "Captain Nemo", _dummyCrewMembers));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("    ")]
+    public void CreateCrewManifest_EmptyOrWhitespaceCaptainName_ShouldThrow(string invalidName)
+    {
+        Assert.Throws<BusinessRuleValidationException>(() =>
+            new CrewManifest(1, invalidName, _dummyCrewMembers));
+    }
+
+    [Theory]
+    [InlineData("A")]  
+    [InlineData("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")] 
+    public void CreateCrewManifest_CaptainNameLengthOutOfBounds_ShouldThrow(string invalidName)
+    {
+        Assert.Throws<BusinessRuleValidationException>(() =>
+            new CrewManifest(1, invalidName, _dummyCrewMembers));
+    }
+}
