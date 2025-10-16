7a475aaf24990a40b326750a7d54b66feab8b27f
RomeuZouXu <francpok.zouxu4@gmail.com>
2025-10-16 17:45:15 +0100

    DataBase Script for windows added

---
+++ SEM5-PI-WEBAPI/DataBase-Script-Windows.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host "Deleting database and migrations..." -ForegroundColor Red
+
+Remove-Item -Path "SEM5-PI-WEBAPI.db" -ErrorAction SilentlyContinue
+Remove-Item -Path "Migrations" -Recurse -Force -ErrorAction SilentlyContinue
+
+Write-Host "Creating initial migration..." -ForegroundColor Yellow
+dotnet ef migrations add Initial
+
+Write-Host "Applying migration..." -ForegroundColor Cyan
+dotnet ef database update
+
+Write-Host " Database cleaned and recreated!" -ForegroundColor Green
