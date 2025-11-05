a544e3cb842a3dd5db22a6c71c874656626292c6
Emanuel Almeida <1230839@isep.ipp.pt>
2025-11-05 09:18:36 +0000

    [US 3.1.4 - Implementation - Issue #131/#134] New table design.

--- client-ui/src/features/physicalResource/components/PhysicalResourceTable.tsx
+++ client-ui/src/features/physicalResource/components/PhysicalResourceTable.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import type { PhysicalResource } from "../types/physicalResource";
+import { PhysicalResourceStatus } from "../types/physicalResource";
 import "../style/physicalResource.css";
 
 interface PhysicalResourceTableProps {
@@ -7,11 +8,24 @@ interface PhysicalResourceTableProps {
     onDetails: (resource: PhysicalResource) => void;
 }
 
+const getStatusClass = (status: PhysicalResourceStatus | string) => {
+    switch (status) {
+        case PhysicalResourceStatus.Available:
+            return "status-available";
+        case PhysicalResourceStatus.Unavailable:
+            return "status-unavailable";
+        case PhysicalResourceStatus.UnderMaintenance:
+            return "status-undermaintenance";
+        default:
+            return "";
+    }
+};
+
 function PhysicalResourceTable({ resources, onDetails }: PhysicalResourceTableProps) {
     const { t } = useTranslation();
 
     if (resources.length === 0) {
-        return <p>{t("physicalResource.noResourcesFound")}</p>;
+        return <p>{t("noResourcesFound")}</p>;
     }
 
     return (
@@ -30,8 +44,16 @@ function PhysicalResourceTable({ resources, onDetails }: PhysicalResourceTablePr
                 <tr key={resource.id}>
                     <td>{resource.code.value}</td>
                     <td>{resource.description}</td>
-                    <td>{resource.physicalResourceType}</td>
-                    <td>{resource.physicalResourceStatus}</td>
+                    <td>{t(`physicalResource.types.${resource.physicalResourceType}`)}</td>
+
+                    {}
+                    <td>
+                            <span className={`status-pill ${getStatusClass(resource.physicalResourceStatus)}`}>
+                                {t(`physicalResource.status.${resource.physicalResourceStatus}`)}
+                            </span>
+                    </td>
+                    {}
+
                     <td>
                         <button
                             onClick={() => onDetails(resource)}
