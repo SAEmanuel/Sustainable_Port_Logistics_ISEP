2cc3be41f6c875bed1a33b921beecbd1bfffee75
RomeuZouXu <francpok.zouxu4@gmail.com>
2025-11-18 10:53:03 +0000

    [SPRINT2 GENERAL ARQSI #187] - Update dockPage

--- client-ui/src/features/docks/pages/Dock.tsx
+++ client-ui/src/features/docks/pages/Dock.tsx
@@ -766,6 +766,7 @@ export default function DockPage() {
                     ))}
             </div>
 
+            
             {/* SLIDE PANEL */}
             {selected && (
                 <div className="dk-slide">
