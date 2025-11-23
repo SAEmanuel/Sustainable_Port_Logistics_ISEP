873ce6d51288c126741f69f4592590bd5b7e4468
Francisco Santos <1230564@isep.ipp.pt>
2025-11-23 14:03:09 +0000

    Add /api/health endpoint mapping to PlanningScheduling API
    Issue: #187

--- SEM5-PI-PlanningScheduling/Program.cs
+++ SEM5-PI-PlanningScheduling/Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddAuthorization();
 
+
+builder.Services.AddHealthChecks(); 
+
 builder.Services.AddHttpClient<PrologClient>();
 builder.Services.AddHttpClient<DockServiceClient>();
 builder.Services.AddHttpClient<QualificationServiceClient>();
@@ -30,5 +33,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+app.MapHealthChecks("/api/health"); 
 
 app.Run();
