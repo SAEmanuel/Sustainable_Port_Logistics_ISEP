d2f699ffbf73a3cae3fbf7bd7f59bd1520090328
SAEmanuel <1230839@isep.ipp.pt>
2025-12-18 20:33:38 +0000

    [Implementation - Issue #262, #215] ARQSI US 4.1.7 - Update vesselVisitExecutionCode.ts

--- sem5-pi-operations_execution/src/domain/vesselVisitExecution/vesselVisitExecutionCode.ts
+++ sem5-pi-operations_execution/src/domain/vesselVisitExecution/vesselVisitExecutionCode.ts
@@ -1,4 +1,6 @@
 import { ValueObject } from "../../core/domain/ValueObject";
+import { Guard } from "../../core/logic/Guard";
+import { BusinessRuleValidationError } from "../../core/logic/BusinessRuleValidationError";
 
 interface VesselVisitExecutionCodeProps {
     value: string;
@@ -14,9 +16,27 @@ export class VesselVisitExecutionCode extends ValueObject<VesselVisitExecutionCo
         super(props);
     }
 
+    public static isValidFormat(code: string): boolean {
+        const regex = /^VVE\d{4}\d{6}$/;
+        return regex.test(code);
+    }
+
     public static create(value: string): VesselVisitExecutionCode {
+        const guardResult = Guard.againstNullOrUndefined(value, 'vesselVisitExecutionCode');
+        if (!guardResult.succeeded) {
+            throw new BusinessRuleValidationError(
+                "Invalid code",
+                guardResult.message || "Code is required"
+            );
+        }
+
+        if (!this.isValidFormat(value)) {
+            throw new BusinessRuleValidationError(
+                "Invalid VVE code format",
+                "O código deve seguir o padrão VVE + Ano(4) + Sequência(6). Ex: VVE2025000001"
+            );
+        }
+
         return new VesselVisitExecutionCode({ value });
     }
 }
-
-//TODO MANU IMPLEMENTA ESTA MERDA SÓ FIZ ISTO ASSIM SIMPLES PARA PODER USAR NA MINHA
