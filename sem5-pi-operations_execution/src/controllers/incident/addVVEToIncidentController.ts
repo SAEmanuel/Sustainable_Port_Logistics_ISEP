33ec7f084ff916a10d7d38f34a7b6d408d341749
Jorge Ubaldo <1231274@isep.ipp.pt>
2025-12-18 16:53:55 +0000

    [ARQSI- Implementation - #221 & #224] - Incident mark as resolved, add/remove VVE controllers

---
+++ sem5-pi-operations_execution/src/controllers/incident/addVVEToIncidentController.ts
@@ -0,0 +1,46 @@
+import {Inject, Service} from "typedi";
+import {BaseController} from "../../core/infra/BaseController";
+import {Logger} from "winston";
+import IIncidentService from "../../services/IServices/IIncidentService";
+import {BusinessRuleValidationError} from "../../core/logic/BusinessRuleValidationError";
+
+@Service()
+export default class UpdateIncidentController extends BaseController{
+
+    constructor(
+        @Inject("IncidentService") private incidentService: IIncidentService,
+        @Inject("logger") private logger: Logger
+
+    ) {
+        super();
+    }
+
+    protected async executeImpl(): Promise<any> {
+        const incidentCode = this.req.query.incidentCode as string;
+        const vveCode = this.req.query.vveCode as string;
+
+        try {
+            const result = await this.incidentService.addVVEToIncidentAsync(incidentCode,vveCode);
+
+            return this.ok(this.res, result.getValue());
+
+        } catch (e) {
+
+            if (e instanceof BusinessRuleValidationError) {
+                this.logger.warn("Business rule violation while adding vve to Incident", {
+                    message: e.message,
+                    details: e.details
+                });
+
+                return this.clientError(e.message);
+            }
+
+            this.logger.error(
+                "Unexpected error adding vve to incident",
+                { e }
+            );
+
+            return this.fail("Internal server error");
+        }
+    }
+}
