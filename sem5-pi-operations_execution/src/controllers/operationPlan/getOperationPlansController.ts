0b626be968cc3661798d30541a596acd6f111271
SAEmanuel <1230839@isep.ipp.pt>
2025-12-30 10:25:43 +0000

    [Implementation - Issue #262] ARQSI US 4.1.3 - New GET Controller added.

---
+++ sem5-pi-operations_execution/src/controllers/operationPlan/getOperationPlansController.ts
@@ -0,0 +1,33 @@
+import { BaseController } from "../../core/infra/BaseController";
+import { Inject, Service } from "typedi";
+import OperationPlanService from "../../services/operationPlanService";
+import { IOperationPlanDTO } from "../../dto/IOperationPlanDTO";
+
+@Service()
+export default class GetOperationPlansController extends BaseController {
+    constructor(
+        @Inject("OperationPlanService")
+        private service: OperationPlanService
+    ) {
+        super();
+    }
+
+    protected async executeImpl(): Promise<any> {
+        try {
+            const startDate = this.req.query.startDate as string;
+            const endDate = this.req.query.endDate as string;
+            const vessel = this.req.query.vessel as string;
+
+            const result = await this.service.getPlansAsync(startDate, endDate, vessel);
+
+            if (result.isFailure) {
+                return this.clientError(result.errorValue().toString());
+            }
+
+            return this.ok<IOperationPlanDTO[]>(this.res, result.getValue());
+        } catch (e) {
+            // @ts-ignore
+            return this.fail(e);
+        }
+    }
+}
